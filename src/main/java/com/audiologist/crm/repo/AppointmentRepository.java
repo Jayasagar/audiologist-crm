a148225bad58f55c73b43f0b16bfc74dec5c9ed0
Jayasagar Jagirapu <jayasagar@gmail.com>
2016-11-24 09:14:49 +0100

    quick initial repo layer interafces

---
+++ src/main/java/com/audiologist/crm/repo/AppointmentRepository.java
@@ -0,0 +1,10 @@
+package com.audiologist.crm.repo;
+
+import com.audiologist.crm.model.Patient;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface AppointmentRepository extends MongoRepository<Patient, String> {
+
+}
